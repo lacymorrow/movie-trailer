142e36eb7e9573e6a7f9557edb156f75060d3a34
Lacy Morrow <lacymorrow0@gmail.com>
2023-09-15 01:50:45 -0400

    Update index.js

--- index.js
+++ index.js
@@ -52,9 +52,9 @@
 	function getMovieId( search, options ) {
 
 		/* Fetch a Movie ID for querying the TMDB API */
-		const param =  options.videoType === 'tv' ? '/3/search/tv?api_key=': '/3/search/movie?api_key='
+		const path =  options.videoType === 'tv' ? '/3/search/tv?api_key=': '/3/search/movie?api_key='
 
-		const endpoint = 'https://api.themoviedb.org' + encodeURI( param + options.apiKey + '&query=' + search + ( ( options.year === null ) ? '' : '&year=' + options.year ) + ( ( options.language === null ) ? '' : '&language=' + options.language )  )
+		const endpoint = 'https://api.themoviedb.org' + encodeURI( path + options.apiKey + '&query=' + search + ( ( options.year === null ) ? '' : '&year=' + options.year ) + ( ( options.language === null ) ? '' : '&language=' + options.language )  )
 		const result = fetch( endpoint, {
 			method: 'GET'
 		} )
@@ -100,10 +100,10 @@
 	}
 
 	function getTrailer( movieId, options ) {
-		const param =  options.videoType === 'tv' ? '/3/tv/': '/3/movie/'
+		const path =  options.videoType === 'tv' ? '/3/tv/': '/3/movie/'
 
 		/* Fetch single or multiple movie trailers via the TMDB API */
-		const endpoint = 'https://api.themoviedb.org' + encodeURI( param + movieId + '/videos?api_key=' + options.apiKey + ( ( options.language === null ) ? '' : '&language=' + options.language ) )
+		const endpoint = 'https://api.themoviedb.org' + encodeURI( path + movieId + '/videos?api_key=' + options.apiKey + ( ( options.language === null ) ? '' : '&language=' + options.language ) )
 		const result = fetch( endpoint, {
 			method: 'GET'
 		} )
